267c2245b7fcd17f917b05f0ea15b87983519682
Elgiz Abbasov <>
2021-05-24 19:14:27 +0000

    Keep alive added

--- main.py
+++ main.py
@@ -2,18 +2,18 @@ import os
 import discord
 import requests
 import json
-
+from keep_alive import keep_alive
 
 my_secret = os.environ['tkn']
 
 client = discord.Client()
-
 
 def get_quote():
   response = requests.get("https://zenquotes.io/api/random")
   json_data = json.loads(response.text)
   quote = "\"" + json_data[0]['q'] + "\"\n" + json_data[0]['a']
   return quote
+
 
 @client.event
 async def on_ready():
@@ -28,4 +28,5 @@ async def on_message(message):
     quote = get_quote()
     await message.channel.send(quote)
 
+keep_alive()
 client.run(my_secret)
