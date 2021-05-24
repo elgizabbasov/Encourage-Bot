2e4dc67ffe1de31ad88006cb2b55c8325b98f6b4
Elgiz Abbasov <>
2021-05-24 01:51:23 +0000

    Random quotes from zenquotes.io & token update

---
+++ main.py
@@ -0,0 +1,31 @@
+import os
+import discord
+import requests
+import json
+
+
+my_secret = os.environ['tkn']
+
+client = discord.Client()
+
+
+def get_quote():
+  response = requests.get("https://zenquotes.io/api/random")
+  json_data = json.loads(response.text)
+  quote = "\"" + json_data[0]['q'] + "\"\n" + json_data[0]['a']
+  return quote
+
+@client.event
+async def on_ready():
+  print('We have logged in as {0.user}'.format(client))
+
+@client.event 
+async def on_message(message):
+  if message.author == client.user: 
+    return
+
+  if message.content.startswith('$inspire'):
+    quote = get_quote()
+    await message.channel.send(quote)
+
+client.run(my_secret)
